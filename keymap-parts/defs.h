20b7fce9665702c038a3eda18afb3140589fd58b
jkelaty <jkelaty@icloud.com>
2020-07-23 02:13:47 -0400

    Indentation fix

--- keymap-parts/defs.h
+++ keymap-parts/defs.h
@@ -5,11 +5,11 @@
  */
 enum jkelaty_keycodes {
     SWITCH = SAFE_RANGE,
-    DIAL_LI,    // Dial light indicator toggle
-    DIAL_TO,    // Dial mode toggle
-    DIAL_BT,    // Dial button switch
-    DIAL_CW,    // Dial clockwise
-    DIAL_CC     // Dial counter-clockwise
+    DIAL_LI,  // Dial light indicator toggle
+    DIAL_TO,  // Dial mode toggle
+    DIAL_BT,  // Dial button switch
+    DIAL_CW,  // Dial clockwise
+    DIAL_CC   // Dial counter-clockwise
 };
 
 /*
@@ -35,6 +35,6 @@ enum jkelaty_layers {
  * Globals
  */
 int DIAL_SETTING = VOLUME;
-bool DIAL_FLAG = false;
+bool DIAL_CW_FLAG = false;
 bool DIAL_LIGHT = false;
 
